037ce1b488b245034f5464e80f0320308e302901
Dshahi14 <51075837+Dshahi14@users.noreply.github.com>
2019-08-07 16:39:10 +0530

    Create sonar.groovy

---
+++ vars/sonar.groovy
@@ -0,0 +1,6 @@
+def sonar() {
+  sh """mvn sonar:sonar \
+  -Dsonar.projectKey=abc_sonar \
+  -Dsonar.host.url=http://35.239.219.161:9000 \
+  -Dsonar.login=7aa0f5eec8f0ef469314e6b95f4c18ce822b0891"""
+}
