9547937897b46e14cf541d45673da1dcd6168252
Dshahi14 <51075837+Dshahi14@users.noreply.github.com>
2019-08-09 19:12:21 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -13,7 +13,9 @@ def call(Map pipelineParams) {
           stage('properties') {
                 steps {
                     script {
-                    properties.user()
+                          def props = readProperties  file:'${WORKSPACE}/user.properties'
+                          def Var1= props['a']
+                          echo "${Var1}"
                     }
                 }
             }
