babc25d9150085e918017a068b984df3e1d9d91a
Dshahi14 <51075837+Dshahi14@users.noreply.github.com>
2019-08-09 15:33:57 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -29,7 +29,7 @@ def call(Map pipelineParams) {
             stage('sonar code quality'){
                 steps {
                     sh """mvn sonar:sonar \
-                   -Dsonar.projectKey='abc_sonar' \
+                   -Dsonar.projectKey='${projectKey}' \
                    -Dsonar.host.url='http://35.184.178.95:9000' \
                    -Dsonar.login='7aa0f5eec8f0ef469314e6b95f4c18ce822b0891'"""
                    
