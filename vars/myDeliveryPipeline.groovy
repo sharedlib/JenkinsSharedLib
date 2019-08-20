58571f3c15968435d0f87f86490c507b27a60f96
Dshahi14 <51075837+Dshahi14@users.noreply.github.com>
2019-08-20 13:12:12 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -2,7 +2,7 @@
     
     def loadProperties(){
         //checkout scm
-     File propertiesFile = new File('${workspace}/user.properties')
+     File propertiesFile = new File('${env.WORKSPACE}/user.properties')
         propertiesFile.withInputStream{
             properties.load(propertiesFile)
         }
