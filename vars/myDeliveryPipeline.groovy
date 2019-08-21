81027fe5262b6d4abf38d4b75434b6096cf2a0fb
Dshahi14 <51075837+Dshahi14@users.noreply.github.com>
2019-08-21 18:34:59 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -33,10 +33,7 @@ def call() {
                 }
                 steps {
                     script {
-                        def props = readProperties  file:'user.properties'
-                            if("${props['runUnitTestAsGoal']}" == "true") {
-                                sh "mvn ${props['mavenTest']}"
-                   } 
+                        sh "mvn ${props['mavenTest']}"
                 }
             }
          }
@@ -50,29 +47,45 @@ def call() {
             }
             
             stage('sonar code quality'){
+                when { 
+                    expression {
+                        script {
+                            echo "I am entering when expression"
+                            def props = readProperties  file:'user.properties'
+                            if("${props['runSonarAsGoal']}" == "true") 
+                                return true
+                        } 
+                    }
+                }
                 steps {
                     script {
                         def props = readProperties  file:'user.properties'
-                        if("${props['runSonarAsGoal']}" == "true") {
-                                sh """
-                                mvn sonar:sonar \
-                               -Dsonar.projectKey="${props['sonarProjectKey']}" \
-                               -Dsonar.host.url="${props['sonarUrl']}" \
-                               -Dsonar.login="${props['sonarLogin']}" \
-                               -Dsonar.projectName="${props['sonarProjectName']}"
-                               """ 
-                        }
+                            sh """
+                            mvn sonar:sonar \
+                            -Dsonar.projectKey="${props['sonarProjectKey']}" \
+                            -Dsonar.host.url="${props['sonarUrl']}" \
+                            -Dsonar.login="${props['sonarLogin']}" \
+                            -Dsonar.projectName="${props['sonarProjectName']}"
+                             """ 
                     }
                 }
             }            
             
             stage ('Publish Artifacts') {
+                when { 
+                    expression {
+                        script {
+                            echo "I am entering when expression"
+                            def props = readProperties  file:'user.properties'
+                            if("${props['runDeployAsGoal']}" == "true") 
+                                return true
+                        } 
+                    }
+                }
                 steps {
                      script {
                         def props = readProperties  file:'user.properties'
-                            if("${props['runDeployAsGoal']}" == "true") {
-                                sh "mvn ${props['mavenDeploy']}" 
-                       }
+                            sh "mvn ${props['mavenDeploy']}" 
                    } 
                 }
             }
